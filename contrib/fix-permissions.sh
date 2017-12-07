fdd05cb95ab8f64d8e28dbe8667404dc061b82fa
Rasheed Amir <rasheed@aurorasolutions.io>
2017-12-07 22:48:56 +0100

    add comment

--- contrib/fix-permissions.sh
+++ contrib/fix-permissions.sh
@@ -4,6 +4,8 @@
 
 ## source: https://github.com/openshift/jenkins/blob/master/2/contrib/jenkins/fix-permissions
 
+## TODO: maybe its not needed! as it looks its needed for OpenShift
+
 find $1 -exec chgrp 0 {} \;
 find $1 -exec chmod g+rw {} \;
 find $1 -type d -exec chmod g+x {} +
